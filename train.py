bb80c9eab05f53ccb579b363b25123971def9938
Alleluia1 <15503046121@163.com>
2025-05-26 14:14:36 +0800

    改wandb9

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import warnings, os
+import time
 # os.environ["CUDA_VISIBLE_DEVICES"]="-1"    # 代表用cpu训练 不推荐！没意义！ 而且有些模块不能在cpu上跑
 # os.environ["CUDA_VISIBLE_DEVICES"]="0"     # 代表用第一张卡进行训练  0：第一张卡 1：第二张卡
 # 多卡训练参考<使用教程.md>下方常见错误和解决方案
@@ -22,11 +23,11 @@ if __name__ == '__main__':
     model.train(data='/root/autodl-tmp/neudet/data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=300,
+                epochs=200,
                 batch=4, # batchsize 不建议乱动，一般来说4的效果都是最好的，越大的batch效果会很差(经验之谈)
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
                 # device='0,1', # 指定显卡和多卡训练参考<使用教程.md>下方常见错误和解决方案
                 # resume='', # last.pt path
-                project='rtdetr_train',
-                name='exp',
+                project='rtdetra1',
+                name = time.strftime("%Y-%m-%d_%H-%M-%S"),
                 )
